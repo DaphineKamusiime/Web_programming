746b212719159fea4def304ab3877771d28cac22
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2023-02-23 09:03:19 +0300

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,8 @@
+const hostels=["Tupendane","Chronos","Sky courts","David's ark"]
+
+const aar1=[1,3,3,5,6,4]
+
+const arr2=[1,3,3,"BOY",6,"HII"]
+
+console.log(hostels[1]);
+console.log(hostels.length)//length of the array
