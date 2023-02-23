d58012192263b5f87602bba0a5efd470ab0fc9e3
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2023-02-23 09:41:42 +0300

    Update Loops.js

--- Loops.js
+++ Loops.js
@@ -5,4 +5,27 @@ const aar1=[1,3,3,5,6,4]
 const arr2=[1,3,3,"BOY",6,"HII"]
 
 console.log(hostels[1]);
+
 console.log(hostels.length)//length of the array
+
+hostels.push("Sabiiti");
+
+console.log(hostels)
+
+function mentionHostels(){
+    for (let i=0; i<hostels.length;i++){
+        console.log("The hostel is ", (i+1),".",hostels[i])
+    }
+}
+mentionHostels()
+/**
+ 
+let n=100;
+
+let sum=0;
+for(let number= 1; number<=n; number++){
+    console.log("The number output is ", number)
+    sum+=number;
+  }
+console.log("SUM = ",sum)
+*/
